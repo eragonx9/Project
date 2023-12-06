edcd8175222ac70d684037a76efe8811eafc3583
Saumya Saraswat <52417545+MagmaCubes1133@users.noreply.github.com>
2023-12-07 02:48:27 +0530

    Update ClubDetailsTemplate.jsx

--- isdl/src/Components/Club_Template/ClubDetailsTemplate.jsx
+++ isdl/src/Components/Club_Template/ClubDetailsTemplate.jsx
@@ -1,18 +1,26 @@
 // ClubDetailsTemplate.jsx
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Header from '../Header/Header'
+import Footer from '../Footer/Footer'
 
 const ClubDetailsTemplate = ({ clubDetails }) => {
   const { name, contact, otherDetails, image, ClubDetails, email } = clubDetails;
 
   return (
-    <div>
-      <h2>{name}</h2>
-      <img src={image} alt={`${name} Logo`} style={{ maxWidth: '100%' }} />
-      <p>Club Details: {ClubDetails}</p>
-      <p>Email: {email}</p>
-      <p>Contact: {contact}</p>
-      {/* Add other details as needed */}
-    </div>
+    <>
+      <Header/>
+      <div>
+        <h2>{name}</h2>
+        <img src={image} alt={`${name} Logo`} style={{ maxWidth: '100%' }} />
+        <p>Club Details: {ClubDetails}</p>
+        <p>Email: {email}</p>
+        <p>Contact: {contact}</p>
+        {/* Add other details as needed */}
+      </div>
+      <Footer/>
+    </>
   );
 };
 
