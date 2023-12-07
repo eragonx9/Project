00fc542cab9a6352fe89faa09b580bf734531ea5
eragonx9 <21ucs195@lnmiit.ac.in>
2023-12-07 22:26:34 +0530

    dd
    gvdgd

--- isdl/src/Components/Club_Template/ClubData.jsx
+++ isdl/src/Components/Club_Template/ClubData.jsx
@@ -169,7 +169,7 @@ const clubData = {
     Mediacell: {
       name: 'Mediacell',
       image:MediacellImage,
-      ClubDetails: ' official media body of lnmiit ',
+      ClubDetails: ' official media body of lnmiit and alpha ',
       email:'mcell@gmail.com',
       contact: '9231547326'
       
